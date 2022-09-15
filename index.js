d765044a67d671de00458db85972c481fe8ffe19
Sarthak Shirsat <90129271+Sarthak-16XO@users.noreply.github.com>
2022-09-15 20:36:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function addTask() {
+
+    var taskname = document.getElementById("taskname").value;
+    var todo = document.getElementById("todo");
+
+    var todoDiv = document.createElement("div");
+    var todoName = document.createElement("li");
+    var deleteBtn = document.createElement("i");
+   
+    deleteBtn.classList.add("fa-sharp");
+    deleteBtn.classList.add("fa-solid");
+    deleteBtn.classList.add("fa-trash");
+
+    todoName.innerHTML = taskname;
+
+    todoDiv.appendChild(todoName);
+    todoDiv.appendChild(deleteBtn);
+
+    todo.appendChild(todoDiv);
+
+}
+
+var todo = document.getElementById("todo");
+todo.addEventListener("click", removeElement);
+
+function removeElement(event) {
+    const element = event.target;
+    
+    if(element.classList[0] == "fa-sharp"){
+        element.parentElement.remove();
+    }
+}
